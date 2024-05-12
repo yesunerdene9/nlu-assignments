290a96d98124682e59a063f3077c92e49d08a373
yesunerdene <yesuuygls@gmail.com>
2024-05-12 12:44:23 +0200

    part2-2

--- exam/exam/LM/part_2/functions.py
+++ exam/exam/LM/part_2/functions.py
@@ -1,2 +1,14 @@
-# Add the class of your model only
-# Here is where you define the architecture of your model using pytorch
+import torch
+import torch.nn as nn
+
+class VariationalDropout(nn.Module):
+    def __init__(self, dropout):
+        super(VariationalDropout, self).__init__()
+        self.dropout = dropout
+
+    def forward(self, x):
+        if not self.training:
+            return x
+        else:
+            mask = torch.empty_like(x).bernoulli_(1 - self.dropout)
+            return x * mask / (1 - self.dropout)
