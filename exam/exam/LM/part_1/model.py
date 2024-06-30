75ed18ffa2a30a880a74b45107bf38d8d70d0dcc
yesunerdene <yesuuygls@gmail.com>
2024-06-30 15:59:11 +0200

    fix4

--- exam/exam/LM/part_1/model.py
+++ exam/exam/LM/part_1/model.py
@@ -8,7 +8,7 @@ class LM_RNN(nn.Module):
                  pad_index=0, 
                  out_dropout=0.1,
                  emb_dropout=0.1, 
-                 n_layers=1):
+                 n_layers=2):
         super(LM_RNN, self).__init__()
 
         self.embedding = nn.Embedding(output_size, emb_size, padding_idx=pad_index)
