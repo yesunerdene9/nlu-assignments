0ddb069206bf6f04379fffce8767d05c5c825041
yesunerdene <yesuuygls@gmail.com>
2024-07-01 11:56:59 +0200

    fix3

--- exam/exam/LM/part_1/main.py
+++ exam/exam/LM/part_1/main.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
     # optimizer = optim.SGD(model.parameters(), lr=lr, weight_decay=weight_decay, momentum=momentum)
 
     # Task 3 - Replace SGD with AdamW
-    optimizer = optim.AdamW(model.parameters(), lr=lr, weight_decay=weight_decay, momentum=momentum)
+    optimizer = optim.AdamW(model.parameters(), lr=lr, weight_decay=weight_decay)
 
     criterion_train = nn.CrossEntropyLoss(ignore_index=lang.word2id["<pad>"])
     criterion_eval = nn.CrossEntropyLoss(ignore_index=lang.word2id["<pad>"], reduction='sum')
